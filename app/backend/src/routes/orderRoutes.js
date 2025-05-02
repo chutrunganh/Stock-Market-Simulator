3448b91c41c67f631d057c0ce0e85e230f1c5c82
Chu Trung Anh <Anh.CT225564@sis.hust.edu.vn>
2025-05-02 17:34:10 +0700

    Add order middleware

--- app/backend/src/routes/orderRoutes.js
+++ app/backend/src/routes/orderRoutes.js
@@ -1,24 +1,21 @@
 import express from 'express';
 import { createOrder, cancelOrder } from '../controllers/orderController.js';
-import { getOrderBook, getOrderBookUpdates } from '../controllers/orderBookController.js';
 import authMiddleware from '../middlewares/authenticationMiddleware.js';
-import  isTradingSessionMiddleware  from '../middlewares/tradingSessionMiddleware.js';
+import isTradingSessionMiddleware from '../middlewares/tradingSessionMiddleware.js';
+import { validateOrder } from '../middlewares/orderMiddleware.js'; // Import the combined validator
+import pool from '../config/dbConnect.js'; // Import pool for database connection
+
 const router = express.Router();
 
 // Route to create a new order - applies middleware in sequence
-router.post('/createOrder', 
+router.post('/createOrder',
     authMiddleware,
     isTradingSessionMiddleware,
+    validateOrder, // Add the combined validation middleware here
     createOrder);
 
 
-// Route to cacel a specific order by ID
-router.delete('/cancelOrder/:orderId', cancelOrder);
-
-// Route to get the order book data
-router.get('/orderBook', getOrderBook);
-
-// Route to get order book updates since a specific timestamp
-router.get('/orderBook/updates', getOrderBookUpdates);
+// Route to cancel a specific order by ID
+router.delete('/cancelOrder/:orderId', authMiddleware, cancelOrder); // Added authMiddleware
 
 export default router;
