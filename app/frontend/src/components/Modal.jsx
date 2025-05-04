afd6df84ee98c838fb28a6807f52523764c57cd6
Ccyclic <iszcyclic24@gmail.com>
2025-05-04 18:42:35 +0700

    5/4/2025

--- app/frontend/src/components/Modal.jsx
+++ app/frontend/src/components/Modal.jsx
@@ -8,14 +8,15 @@
 import React from 'react';
 import './Modal.css'; // Tạo file CSS cho modal nếu cần
 
-const Modal = ({ isOpen, onClose, children }) => {
+const Modal = ({ isOpen, onClose, children, className }) => {
     if (!isOpen) return null;
 
     return (
         <div className="modal-overlay" onClick={onClose}>
-            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                {/* Xóa nút close-button */}
-                {/* <button className="close-button" onClick={onClose}>X</button> */}
+            <div 
+                className={`modal-content ${className || ''}`} 
+                onClick={e => e.stopPropagation()}
+            >
                 {children}
             </div>
         </div>
