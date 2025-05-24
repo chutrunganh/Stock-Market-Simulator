82998ed025ef69e6ff675d9f0b7dc11c026a6b69
PhMtien <Tien.PM225555@sis.hust.edu.vn>
2025-05-24 20:36:41 +0700

    add the cardgraph

--- app/frontend/src/pages/Home/HomeComponents/CardGraph.jsx
+++ app/frontend/src/pages/Home/HomeComponents/CardGraph.jsx
@@ -4,9 +4,22 @@ import stockGif from '../../../assets/images/stock.gif';
 
 function CardGraph(){    return(
         <div className="CardGraph">
-            <img src="https://fakeimg.pl/400x300" alt="Placeholder" />
-            <img src={stockGif} alt="Stock Animation" width="400" height="300" style={{ objectFit: 'contain' }} />
-            <img src="https://fakeimg.pl/400x300" alt="Placeholder" />
+            <a href="https://hust.edu.vn/vi/news/khoa-hoc-cong-nghe-dmst/dai-hoc-bach-khoa-ha-noi-dam-nghi-lon-hanh-dong-lon-655458.html" target="_blank" rel="noopener noreferrer">
+                <img src="hust1.jpg" alt="hust new" />
+                <div className="card-title">Đại học Bách khoa Hà Nội dám nghĩ lớn, hành động lớn!</div>
+                <div className="card-date">20/05/2025</div>
+            </a>
+            <a href="https://hust.edu.vn/vi/news/tin-tuc-su-kien/ban-linh-dot-pha-de-chinh-phuc-cac-muc-tieu-655443.html" target="_blank" rel="noopener noreferrer">
+                <img src="hust2.jpg" alt="hust new" />
+                <div className="card-title">Bản lĩnh, đột phá để chinh phục các mục tiêu</div>
+                <div className="card-date">18/05/2025</div>
+            </a>
+            <a href="https://vietstock.vn/2025/05/vang-the-gioi-tang-hon-2-sau-dong-thai-cua-ong-trump-759-1311744.htm" target="_blank" rel="noopener noreferrer">
+                <img src="gold.jpeg" alt="hust new" />
+                <div className="card-title">Vàng thế giới tăng hơn 2% sau động thái của ông Trump</div>
+                <div className="card-date">24/05/2025</div>
+            </a>
+            
         </div>
     );
 }
